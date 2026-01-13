94bdfc935f6285b5add5a186027f4e7ee099d22a
ychhh <3240532664@qq.com>
2026-01-14 01:30:18 +0800

    添加新的类Person两个属性

--- src/text01.java
+++ src/text01.java
@@ -2,5 +2,9 @@ public class text01 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("java");
+        Person p1 = new Person(10,"zhansan");
+        p1.introduce();
+        Person p2 = new Person(12,"lisi");
+        p2.introduce();
     }
 }
